530ea8ff50f6f8a251adcfc6164ce5abc0346fe1
Luke <constableldp@gmail.com>
2021-03-04 16:03:01 +0000

    matrix class completed

--- matrix/src/matrix.py
+++ matrix/src/matrix.py
@@ -1,9 +1,28 @@
 class Matrix:
     def __init__(self, matrix_string):
-        pass
+        self.matrix_string = matrix_string
 
-    def row(self, index):
-        pass
+    def row(self):
+        row_strings = self.matrix_string.split("\n")
+        rows = [row_string.split(" ") for row_string in row_strings]
+        for index in range(len(rows)):
+            rows[index] = [int(number) for number in rows[index]]
+        return rows
 
-    def column(self, index):
-        pass
+
+    # def column(self):
+    #     rows = self.row()
+    #     columns = rows
+    #     for index in range(len(rows)):
+    #         print(index)
+    #         columns[index] = [rows[number][index] for number in range(len(rows))]
+    #     return columns
+
+    def column(self):
+        rows = self.row()
+        columns = []
+        for index in range(len(rows[0])):
+            column = [(row[index]) for row in rows]
+            columns.append(column)
+            column = []
+        return columns
--- matrix/tests/matrix_test.py
+++ matrix/tests/matrix_test.py
@@ -4,10 +4,24 @@ from src.matrix import Matrix
 
 class MatrixTest(unittest.TestCase):
     # Tests
+    def setUp(self):
+        self.matrix = Matrix("9 8 7\n5 3 2\n6 6 7")
 
-    def test_extract_row_from_one_number_matrix(self):
-        matrix = Matrix("1")
-        self.assertEqual(matrix.row(1), [1])
+
+    # def test_extract_row_from_one_number_matrix(self):
+    #     matrix = Matrix("1")
+    #     self.assertEqual(matrix.row(1), [1])
+
+    def test_rows(self):
+        self.assertEqual([[9, 8, 7], [5, 3, 2], [6, 6, 7]], self.matrix.row())
+
+    def test_columns__1(self):
+        self.assertEqual([[9, 5, 6], [8, 3, 6], [7, 2, 7]], self.matrix.column())
+
+    def test_matrix(self):
+        new_matrix = Matrix("9 8 7 3\n5 3 2 3\n6 6 7 3")
+        self.assertEqual([[9, 8, 7, 3], [5, 3, 2, 3], [6, 6, 7, 3]], new_matrix.row())
+        self.assertEqual([[9, 5, 6], [8, 3, 6], [7, 2, 7], [3, 3, 3]], new_matrix.column())
 
     # Test can extract a given row
 
